7ee838a4a7c39d4c55fa87ba6627371c700ed93c
Max Stenke <max_stenke@yahoo.de>
2018-11-15 15:15:56 +0100

    Update id

--- app/src/main/java/topgrost/mocoquizer/MainActivity.java
+++ app/src/main/java/topgrost/mocoquizer/MainActivity.java
@@ -2,12 +2,23 @@ package topgrost.mocoquizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newGame = findViewById(R.id.btnNewGame);
+        newGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
