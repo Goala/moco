50723964c22cf95dad488a10d00c3d1b2e38928c
Lukas Gross <lu851gro@htwg-konstanz.de>
2018-12-01 13:49:21 +0100

    QuizActivity: use relativ layout, removed hardcoded sizes and positions

--- eq-app/app/src/main/java/topgrost/mocoquizer/quiz/QuizActivity.java
+++ eq-app/app/src/main/java/topgrost/mocoquizer/quiz/QuizActivity.java
@@ -6,8 +6,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import topgrost.mocoquizer.R;
+import topgrost.mocoquizer.quiz.model.Quiz;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -16,12 +18,21 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_quiz);
 
-        Button btnAddQuestion = findViewById(R.id.btnCreateQuiz);
+        Button btnAddQuestion = findViewById(R.id.btnAddQuiz);
         btnAddQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO add Question Container to layout
             }
         });
+    }
+
+    private void save(){
+        Quiz quiz = new Quiz();
+
+        final TextView tvName = findViewById(R.id.name);
+        quiz.setName(tvName.getText().toString());
+
+        // TODO generate json and save in firebase
     }
 }
