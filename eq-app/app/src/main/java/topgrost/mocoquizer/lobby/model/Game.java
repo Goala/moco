b518cddff1c8bee773e19dc2a5058bb5fe503045
Lukas Groß <lu851gro@htwg-konstanz.de>
2018-12-13 13:20:53 +0100

    updated model class for Game

--- eq-app/app/src/main/java/topgrost/mocoquizer/lobby/model/Game.java
+++ eq-app/app/src/main/java/topgrost/mocoquizer/lobby/model/Game.java
@@ -1,17 +1,85 @@
 package topgrost.mocoquizer.lobby.model;
 
-import com.google.firebase.database.IgnoreExtraProperties;
+import java.io.Serializable;
+import java.util.Objects;
 
-@IgnoreExtraProperties
-public class Game {
+public class Game implements Serializable {
 
-    public String name;
+    private String name;
+    private String password;
+    private String deviceId;
+    private String quizId;
+    private boolean running;
 
     public Game() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public Game(String name) {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getQuizId() {
+        return quizId;
+    }
+
+    public void setQuizId(String quizId) {
+        this.quizId = quizId;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return running == game.running &&
+                Objects.equals(name, game.name) &&
+                Objects.equals(password, game.password) &&
+                Objects.equals(deviceId, game.deviceId) &&
+                Objects.equals(quizId, game.quizId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, password, deviceId, quizId, running);
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", deviceId='" + deviceId + '\'' +
+                ", quizId='" + quizId + '\'' +
+                ", running=" + running +
+                '}';
     }
 }
