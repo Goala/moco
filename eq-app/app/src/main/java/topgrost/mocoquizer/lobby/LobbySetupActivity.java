6497f2dca469a2fe54f3f21da7ceda4197e3e869
Lukas Groß <lu851gro@htwg-konstanz.de>
2018-12-13 13:27:11 +0100

    LobbySetupActivity: save game data in firebase

--- eq-app/app/src/main/java/topgrost/mocoquizer/lobby/LobbySetupActivity.java
+++ eq-app/app/src/main/java/topgrost/mocoquizer/lobby/LobbySetupActivity.java
@@ -6,7 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import topgrost.mocoquizer.R;
+import topgrost.mocoquizer.lobby.model.Game;
 
 public class LobbySetupActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,13 +21,23 @@ public class LobbySetupActivity extends AppCompatActivity implements View.OnClic
 
         setContentView(R.layout.lobby_setup);
 
-        Button btnStartGame = findViewById(R.id.lobbySetupStartGame);
+        Button btnStartGame = findViewById(R.id.lobbySetupCreate);
         btnStartGame.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        // TODO store data in firebase
+        Game game = new Game();
+        game.setRunning(false);
+        game.setName(((TextView) findViewById(R.id.lobbySetupName)).getText().toString());
+        game.setPassword(((TextView) findViewById(R.id.lobbySetupPassword)).getText().toString());
+        game.setDeviceId(((Spinner) findViewById(R.id.lobbySetupDevice)).getSelectedItem().toString());
+        game.setQuizId(((Spinner) findViewById(R.id.lobbySetupQuiz)).getSelectedItem().toString());
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference gameRef = database.getReference(Game.class.getSimpleName().toLowerCase());
+        gameRef.push().setValue(game);
+
         startActivity(new Intent(getApplicationContext(), LobbyActivity.class));
     }
 }
