6538f68e308025725c0d0a675bfa03f1ec174d2a
Lukas Groß <lu851gro@htwg-konstanz.de>
2018-12-13 13:42:01 +0100

    LobbySetupActivity: work on loading device/quiz data

--- eq-app/app/src/main/java/topgrost/mocoquizer/lobby/LobbySetupActivity.java
+++ eq-app/app/src/main/java/topgrost/mocoquizer/lobby/LobbySetupActivity.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.*;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import topgrost.mocoquizer.R;
+import topgrost.mocoquizer.lobby.model.Device;
 import topgrost.mocoquizer.lobby.model.Game;
+import topgrost.mocoquizer.quiz.model.Quiz;
 
 public class LobbySetupActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,8 +21,34 @@ public class LobbySetupActivity extends AppCompatActivity implements View.OnClic
 
         setContentView(R.layout.lobby_setup);
 
+        loadAndSetDeviceList();
+        loadAndSetQuizList();
+
         Button btnStartGame = findViewById(R.id.lobbySetupCreate);
         btnStartGame.setOnClickListener(this);
+    }
+
+    private void loadAndSetDeviceList() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference deviceRef = database.getReference(Device.class.getSimpleName().toLowerCase());
+
+        Spinner spinner = findViewById(R.id.lobbySetupDevice);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getBaseContext(),
+                android.R.layout.simple_spinner_dropdown_item, new String[]{"wer23489_Arduino Mega"});
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+    }
+
+    private void loadAndSetQuizList() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference quizRef = database.getReference(Quiz.class.getSimpleName().toLowerCase());
+
+        Spinner spinner = findViewById(R.id.lobbySetupDevice);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getBaseContext(),
+            android.R.layout.simple_spinner_dropdown_item, new String[]{"df234ert_Quiz 1"});
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     @Override
---
+++ eq-app/app/src/main/java/topgrost/mocoquizer/lobby/model/Device.java
@@ -0,0 +1,6 @@
+package topgrost.mocoquizer.lobby.model;
+
+import java.io.Serializable;
+
+public class Device implements Serializable {
+}
