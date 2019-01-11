cecfe1b8aa635228e816e469d38003e537dcf0c1
Lukas Groß <lu851gro@htwg-konstanz.de>
2019-01-11 16:16:39 +0100

    QuestionEditorFragment: added speech 2 text for question

--- eq-app/app/src/main/java/topgrost/mocoquizer/quiz/view/QuestionEditorFragment.java
+++ eq-app/app/src/main/java/topgrost/mocoquizer/quiz/view/QuestionEditorFragment.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,12 +19,16 @@ import android.widget.CheckBox;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import topgrost.mocoquizer.R;
 import topgrost.mocoquizer.quiz.QuizEditorActivity;
 import topgrost.mocoquizer.model.Answer;
 import topgrost.mocoquizer.model.Question;
 
-public class QuestionEditorFragment extends Fragment implements View.OnClickListener {
+public class QuestionEditorFragment extends Fragment implements View.OnClickListener, RecognitionListener {
+
+    private SpeechRecognizer speech = SpeechRecognizer.createSpeechRecognizer(getActivity());
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,11 +50,22 @@ public class QuestionEditorFragment extends Fragment implements View.OnClickList
 
         final Button btnSave = view.findViewById(R.id.quizEditorApply);
         btnSave.setOnClickListener(this);
+
+        speech.setRecognitionListener(this);
+
+        Intent recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "de");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getActivity().getPackageName());
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+        speech.startListening(recognizerIntent);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+
+        speech.stopListening();
 
         InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
@@ -93,5 +112,52 @@ public class QuestionEditorFragment extends Fragment implements View.OnClickList
         question.getAnswers().add(answerTwo);
         question.getAnswers().add(answerThree);
         return question;
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle params) {
+
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onError(int error) {
+
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        final TextView questionTextView = getView().findViewById(R.id.quizEditorQuestionText);
+        questionTextView.setText(matches.get(0));
+    }
+
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+
+    }
+
+    @Override
+    public void onEvent(int eventType, Bundle params) {
+
     }
 }
